a479cb56c5b7dc6a826ee80c7e562877ea53ebbf
Alex Chechetkin <chechetkin.aa@phystech.edu>
2023-10-31 23:31:22 +0300

    Caesar cipher is improved and Vigenere cipher is done

--- src/vigenere.py
+++ src/vigenere.py
@@ -1,12 +1,12 @@
 import string
 
 
-class Caesar:
-    shift = 0
+class Vigenere:
+    key = ""
     alphabet_size = 26
 
-    def __init__(self, shift):
-        self.shift = shift % self.alphabet_size
+    def __init__(self, key):
+        self.key = key
 
     def transform_line(self, line, mode):
         """
@@ -14,6 +14,7 @@ class Caesar:
         mode = 2 -- decrypt
         """
         output_line = ""
+        count = 0
         for letter in line:
             if letter not in string.ascii_lowercase and letter not in string.ascii_uppercase:
                 output_line += letter
@@ -26,13 +27,16 @@ class Caesar:
                 modified_position = ""
 
                 if mode == 1:
-                    modified_position = (ord(letter) - ord("A") + self.shift) % self.alphabet_size
+                    modified_position = (ord(letter) + ord(self.key[count])) % self.alphabet_size
                 elif mode == 2:
-                    modified_position = (ord(letter) - ord("A") - self.shift) % self.alphabet_size
+                    modified_position = (ord(letter) - ord(
+                        self.key[count]) + self.alphabet_size) % self.alphabet_size
 
                 new_letter = chr(modified_position + ord("A"))
                 if is_lower:
                     new_letter = new_letter.lower()
                 output_line += new_letter
+
+                count = (count + 1) % len(self.key)
 
         return output_line
