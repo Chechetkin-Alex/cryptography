90cf0951760728d88b237a2bf18535d3194e912e
Alex Chechetkin <chechetkin.aa@phystech.edu>
2023-11-04 15:39:25 +0300

    Caesar cipher and Vigenere cipher are improved

--- src/caesar.py
+++ src/caesar.py
@@ -8,31 +8,31 @@ class Caesar:
     def __init__(self, shift):
         self.shift = shift % self.alphabet_size
 
-    def transform_line(self, line, mode):
+    def transform(self, message, mode):
         """
         mode = 1 -- encrypt
         mode = 2 -- decrypt
         """
-        output_line = ""
-        for letter in line:
+        output = ""
+        for letter in message:
             if letter not in string.ascii_lowercase and letter not in string.ascii_uppercase:
-                output_line += letter
+                output += letter
             else:
                 is_lower = False
                 if letter.islower():
                     is_lower = True
                     letter = letter.upper()
 
-                modified_position = ""
-
                 if mode == 1:
                     modified_position = (ord(letter) - ord("A") + self.shift) % self.alphabet_size
                 elif mode == 2:
                     modified_position = (ord(letter) - ord("A") - self.shift) % self.alphabet_size
+                else:
+                    raise ValueError(f"Incorrect {mode=}, should be 1 or 2")
 
                 new_letter = chr(modified_position + ord("A"))
                 if is_lower:
                     new_letter = new_letter.lower()
-                output_line += new_letter
+                output += new_letter
 
-        return output_line
+        return output
--- src/vigenere.py
+++ src/vigenere.py
@@ -8,35 +8,35 @@ class Vigenere:
     def __init__(self, key):
         self.key = key
 
-    def transform_line(self, line, mode):
+    def transform(self, message, mode):
         """
         mode = 1 -- encrypt
         mode = 2 -- decrypt
         """
-        output_line = ""
+        output = ""
         count = 0
-        for letter in line:
+        for letter in message:
             if letter not in string.ascii_lowercase and letter not in string.ascii_uppercase:
-                output_line += letter
+                output += letter
             else:
                 is_lower = False
                 if letter.islower():
                     is_lower = True
                     letter = letter.upper()
 
-                modified_position = ""
-
                 if mode == 1:
                     modified_position = (ord(letter) + ord(self.key[count])) % self.alphabet_size
                 elif mode == 2:
                     modified_position = (ord(letter) - ord(
                         self.key[count]) + self.alphabet_size) % self.alphabet_size
+                else:
+                    raise ValueError(f"Incorrect {mode=}, should be 1 or 2")
 
                 new_letter = chr(modified_position + ord("A"))
                 if is_lower:
                     new_letter = new_letter.lower()
-                output_line += new_letter
+                output += new_letter
 
                 count = (count + 1) % len(self.key)
 
-        return output_line
+        return output
